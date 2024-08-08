101613b0b5d1ffd0d5adba66fab3a95a5b8bd75c
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-08-08 23:52:52 +0530

    update

--- Services/UserService/src/controllers/User.controller.js
+++ Services/UserService/src/controllers/User.controller.js
@@ -2,56 +2,335 @@ import User from "../models/User.schema.js";
 import bcrypt from "bcrypt";
 import moment from "moment-timezone";
 
+// Sign up a new user
 async function signUp(req, res) {
 	try {
-		const { userName, password, name } = req?.body;
+		const { userName, password, name } = req.body;
 		console.log(userName, password, name);
-		const user = await User.findOne({ userName: userName });
-		if (user) throw new Error("User already exist");
-		const hashPassword = await bcrypt.hash(password, 10);
+
+		// Check if the user already exists
+		const existingUser = await User.findOne({ userName });
+		if (existingUser) throw new Error("User already exists");
+
+		// Hash the password before saving
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// Create a new user instance
 		const newUser = new User({
-			userName: userName,
-			password: hashPassword,
-			name: name,
+			userName,
+			password: hashedPassword,
+			name,
 		});
+
+		// Save the user to the database
 		await newUser.save();
-		res.status(200).send({ status: true, user: newUser.userName });
+
+		res.status(200).send({ status: true, data: { user: newUser.userName } });
 	} catch (error) {
 		console.log(error.message);
 		res.status(400).send({ status: false, error: error.message });
 	}
 }
 
+// Sign in an existing user
 async function signIn(req, res) {
 	try {
-		const { userName, password } = req?.body;
+		const { userName, password } = req.body;
 		console.log(userName, password);
-		const user = await User.findOne({ userName: userName });
+
+		// Find the user by userName
+		const user = await User.findOne({ userName });
 		if (!user) throw new Error("User doesn't exist");
-		if (!(await bcrypt.compare(password, user.password)))
-			throw new Error("Invalid credentials");
+
+		// Compare the provided password with the stored hash
+		const isPasswordValid = await bcrypt.compare(password, user.password);
+		if (!isPasswordValid) throw new Error("Invalid credentials");
+
+		// Update lastSeen timestamp
 		await updateLastSeen(req, res, false);
-		res.status(200).send({ status: true, user: user.userName });
+
+		res.status(200).send({ status: true, data: { user: user.userName } });
 	} catch (error) {
 		console.log(error.message);
 		res.status(400).send({ status: false, error: error.message });
 	}
 }
 
+// Update lastSeen timestamp for a user
 async function updateLastSeen(req, res, isHTTP = true) {
 	try {
-		const { userName } = req?.body;
+		const { userName } = req.body;
 		console.log(userName);
-		const user = await User.findOne({ userName: userName });
+
+		// Find the user by userName
+		const user = await User.findOne({ userName });
 		if (!user) throw new Error("User doesn't exist");
+
+		// Update lastSeen to the current time
 		user.lastSeen = moment().tz("Asia/Kolkata").toDate();
-		user.save();
-		if (isHTTP) res.status(200).send({ status: true, user: user.userName });
-		return;
+		await user.save();
+
+		if (isHTTP) {
+			res.status(200).send({
+				status: true,
+				data: { user: user.userName, lastSeen: user.lastSeen },
+			});
+		}
 	} catch (error) {
 		console.log(error.message);
 		res.status(400).send({ status: false, error: error.message });
 	}
 }
 
-export { signUp, signIn, updateLastSeen };
+// Get lastSeen timestamp for a user
+async function getLastSeen(req, res) {
+	try {
+		const { userName } = req.query;
+		console.log(userName);
+
+		// Find the user by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User doesn't exist");
+
+		res.status(200).send({
+			status: true,
+			data: { user: user.userName, lastSeen: user.lastSeen },
+		});
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+// Add a friend for a user
+async function addFriend(req, res) {
+	try {
+		const { userName, friendUserName } = req.body;
+
+		// Check if the user and friend are the same person
+		if (userName === friendUserName)
+			throw new Error("User cannot add themselves as a friend.");
+
+		// Find the user and the friend by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User does not exist.");
+
+		const friend = await User.findOne({ userName: friendUserName });
+		if (!friend) throw new Error("Friend does not exist.");
+
+		// Check if the friend is already in the user's friend list
+		if (user.friends.includes(friend._id)) {
+			throw new Error("This user is already in your friend list.");
+		}
+
+		// Add each other as friends
+		user.friends.push(friend._id);
+		friend.friends.push(user._id);
+
+		// Save both users' updated data
+		await Promise.all([user.save(), friend.save()]);
+
+		res
+			.status(200)
+			.send({ status: true, message: "Friend added successfully." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+// Remove a friend for a user
+async function removeFriend(req, res) {
+	try {
+		const { userName, friendUserName } = req.body;
+
+		// Check if the user and friend are the same person
+		if (userName === friendUserName)
+			throw new Error("User cannot remove themselves as a friend.");
+
+		// Find the user and the friend by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User does not exist.");
+
+		const friend = await User.findOne({ userName: friendUserName });
+		if (!friend) throw new Error("Friend does not exist.");
+
+		// Check if the friend is in the user's friend list
+		if (!user.friends.includes(friend._id)) {
+			throw new Error("This user is not in your friend list.");
+		}
+
+		// Remove each other as friends
+		user.friends.pull(friend._id);
+		friend.friends.pull(user._id);
+
+		// Save both users' updated data
+		await Promise.all([user.save(), friend.save()]);
+
+		res
+			.status(200)
+			.send({ status: true, message: "Friend removed successfully." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+// Block a user for a user
+async function blockUser(req, res) {
+	try {
+		const { userName, blockUserName } = req.body;
+		console.log(userName, blockUserName);
+
+		// Check if the user and blockUser are the same person
+		if (userName === blockUserName)
+			throw new Error("User cannot block themselves.");
+
+		// Find the user and the blockUser by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User does not exist.");
+
+		const blockUser = await User.findOne({ userName: blockUserName });
+		if (!blockUser) throw new Error("User to be blocked does not exist.");
+
+		// Check if the user is already blocked
+		if (user.blockedUsers.includes(blockUser._id)) {
+			throw new Error("This user is already blocked.");
+		}
+
+		// Block the user
+		user.blockedUsers.push(blockUser._id);
+
+		// Save the updated user data
+		await user.save();
+
+		res
+			.status(200)
+			.send({ status: true, message: "User blocked successfully." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+// Unblock a user for a user
+async function unblockUser(req, res) {
+	try {
+		const { userName, unblockUserName } = req.body;
+		console.log(userName, unblockUserName);
+
+		// Check if the user and unblockUser are the same person
+		if (userName === unblockUserName)
+			throw new Error("User cannot unblock themselves.");
+
+		// Find the user and the unblockUser by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User does not exist.");
+
+		const unblockUser = await User.findOne({ userName: unblockUserName });
+		if (!unblockUser) throw new Error("User to be unblocked does not exist.");
+
+		// Check if the user is already unblocked
+		if (!user.blockedUsers.includes(unblockUser._id)) {
+			throw new Error("This user is not in your blocked list.");
+		}
+
+		// Unblock the user
+		user.blockedUsers.pull(unblockUser._id);
+
+		// Save the updated user data
+		await user.save();
+
+		res
+			.status(200)
+			.send({ status: true, message: "User unblocked successfully." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+// Block a group for a user
+async function blockGroup(req, res) {
+	try {
+		const { userName, blockUserName } = req.body;
+		console.log(userName, blockUserName);
+
+		// Check if the user and blockUser are the same person
+		if (userName === blockUserName)
+			throw new Error("User cannot block themselves.");
+
+		// Find the user and the blockUser by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User does not exist.");
+
+		const blockUser = await User.findOne({ userName: blockUserName });
+		if (!blockUser) throw new Error("User to be blocked does not exist.");
+
+		// Check if the user is already blocked
+		if (user.blockedUsers.includes(blockUser._id)) {
+			throw new Error("This user is already blocked.");
+		}
+
+		// Block the user
+		user.blockedUsers.push(blockUser._id);
+
+		// Save the updated user data
+		await user.save();
+
+		res
+			.status(200)
+			.send({ status: true, message: "User blocked successfully." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+// Unblock a group for a user
+async function unblockGroup(req, res) {
+	try {
+		const { userName, unblockUserName } = req.body;
+		console.log(userName, unblockUserName);
+
+		// Check if the user and unblockUser are the same person
+		if (userName === unblockUserName)
+			throw new Error("User cannot unblock themselves.");
+
+		// Find the user and the unblockUser by userName
+		const user = await User.findOne({ userName });
+		if (!user) throw new Error("User does not exist.");
+
+		const unblockUser = await User.findOne({ userName: unblockUserName });
+		if (!unblockUser) throw new Error("User to be unblocked does not exist.");
+
+		// Check if the user is already unblocked
+		if (!user.blockedUsers.includes(unblockUser._id)) {
+			throw new Error("This user is not in your blocked list.");
+		}
+
+		// Unblock the user
+		user.blockedUsers.pull(unblockUser._id);
+
+		// Save the updated user data
+		await user.save();
+
+		res
+			.status(200)
+			.send({ status: true, message: "User unblocked successfully." });
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).send({ status: false, error: error.message });
+	}
+}
+
+export {
+	signUp,
+	signIn,
+	updateLastSeen,
+	getLastSeen,
+	addFriend,
+	removeFriend,
+	blockUser,
+	unblockUser,
+};
--- Services/UserService/src/routes/User.routes.js
+++ Services/UserService/src/routes/User.routes.js
@@ -3,12 +3,23 @@ import {
 	signUp,
 	signIn,
 	updateLastSeen,
+	getLastSeen,
+	addFriend,
+	removeFriend,
+	blockUser,
+	unblockUser,
 } from "../controllers/User.controller.js";
 
 const router = Router();
 
+router.get("/get-last-seen", getLastSeen);
+
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
 router.post("/update-last-seen", updateLastSeen);
+router.post("/add-friend", addFriend);
+router.post("/remove-friend", removeFriend);
+router.post("/block-user", blockUser);
+router.post("/unblock-user", unblockUser);
 
 export default router;
