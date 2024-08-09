f65af8816bb989c0cb19ac007fb35474c30d1aa5
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-08-09 16:31:22 +0530

    add authorization

--- APIGateway/app.js
+++ APIGateway/app.js
@@ -2,12 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { verifyJWT } from "./middlewares/Authorization.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		credentials: true,
+	})
+);
+app.use(cookieParser());
 app.use((req, res, next) => {
 	if (req.path.includes("sign")) return next();
 	verifyJWT(req, res, next);
--- APIGateway/middlewares/Authorization.js
+++ APIGateway/middlewares/Authorization.js
@@ -1,105 +1,109 @@
 import jwt from "jsonwebtoken";
-import { signJWT } from "../utils/generateJWT.utils.js";
+import axios from "axios";
 
 async function verifyJWT(req, res, next) {
-	console.log(req.path);
+	try {
+		const { ACCESS_TOKEN, REFRESH_TOKEN } = req.cookies;
 
-	// try {
-	// 	const ACCESS_TOKEN = req.cookies.ACCESS_TOKEN;
-	// 	const REFRESH_TOKEN = req.cookies.REFRESH_TOKEN;
+		console.log(
+			"Access Token:",
+			ACCESS_TOKEN,
+			"\nRefresh Token:",
+			REFRESH_TOKEN
+		);
 
-	// 	console.log(
-	// 		"Access Token:",
-	// 		ACCESS_TOKEN,
-	// 		"\n",
-	// 		"Refresh Token:",
-	// 		REFRESH_TOKEN
-	// 	);
+		if (!ACCESS_TOKEN) {
+			throw new Error("Access token is missing");
+		}
 
-	// 	const decodedAccessToken = jwt.verify(
-	// 		ACCESS_TOKEN,
-	// 		process.env.ACCESS_TOKEN_SECRET
-	// 	);
+		const decodedAccessToken = jwt.verify(
+			ACCESS_TOKEN,
+			process.env.ACCESS_TOKEN_SECRET
+		);
 
-	// 	console.log("Decoded Access Token:", decodedAccessToken);
+		console.log("Decoded Access Token:", decodedAccessToken);
 
-	// 	if (decodedAccessToken == null) {
-	// 		return res.status(401).send("Not authorized");
-	// 	}
+		if (!decodedAccessToken) {
+			return res.status(401).send("Not authorized");
+		}
 
-	// 	const response = await axios.get(
-	// 		`http://localhost:3008/api/v1/session/get-session?userName=${decodedAccessToken.userName}`
-	// 	);
+		const { data: session } = await axios.get(
+			`http://localhost:3008/api/v1/session/get-session?userName=${decodedAccessToken.userName}`
+		);
 
-	// 	const session = response.data;
+		if (!session.status) throw new Error("Session not found");
 
-	// 	if (!session.status) throw new Error("Session not found");
+		req.userName = decodedAccessToken.userName;
+		console.log(req.userName);
 
-	// 	req.user = decodedAccessToken.user;
-	// 	next();
-	// } catch (error) {
-	// 	console.log("Access token verification failed:", error.message);
+		req.headers["X-User-Name"] = decodedAccessToken.userName;
 
-	// 	try {
-	// 		const REFRESH_TOKEN = req.cookies.REFRESH_TOKEN;
-	// 		const decodedRefreshToken = jwt.verify(
-	// 			REFRESH_TOKEN,
-	// 			process.env.REFRESH_TOKEN_SECRET
-	// 		);
+		return next();
+	} catch (accessError) {
+		console.log("Access token verification failed:", accessError.message);
 
-	// 		const response = await axios.get(
-	// 			`http://localhost:3008/api/v1/session/get-session?refreshToken=${REFRESH_TOKEN}`
-	// 		);
+		try {
+			if (!REFRESH_TOKEN) {
+				throw new Error("Refresh token is missing");
+			}
 
-	// 		const current_session = response.data;
+			const decodedRefreshToken = jwt.verify(
+				REFRESH_TOKEN,
+				process.env.REFRESH_TOKEN_SECRET
+			);
 
-	// 		if (!current_session.status) {
-	// 			throw new Error("Session not found");
-	// 		}
+			const { data: currentSession } = await axios.get(
+				`http://localhost:3008/api/v1/session/get-session?refreshToken=${REFRESH_TOKEN}`
+			);
 
-	// 		if (current_session.userName !== decodedRefreshToken.userName) {
-	// 			throw new Error("User mismatch");
-	// 		}
+			if (!currentSession.status) {
+				throw new Error("Session not found");
+			}
 
-	// 		console.log("Current session:", current_session);
+			if (currentSession.userName !== decodedRefreshToken.userName) {
+				throw new Error("User mismatch");
+			}
 
-	// 		const newAccessToken = signJWT(current_session.userName);
+			console.log("Current session:", currentSession);
 
-	// 		res.cookie("ACCESS_TOKEN", newAccessToken, {
-	// 			httpOnly: true,
-	// 			sameSite: "None",
-	// 			secure: true,
-	// 			expires: new Date(Date.now() + 86400 * 1000),
-	// 		});
+			const newAccessToken = signJWT(currentSession.userName);
 
-	// 		req.user = decodedRefreshToken.user;
+			res.cookie("ACCESS_TOKEN", newAccessToken, {
+				httpOnly: true,
+				sameSite: "None",
+				secure: true,
+				expires: new Date(Date.now() + 86400 * 1000), // 1 day
+			});
 
-	// 		next();
-	// 	} catch (refreshError) {
-	// 		console.log("Refresh token verification failed:", refreshError.message);
+			req.userName = decodedRefreshToken.userName;
+			return next();
+		} catch (refreshError) {
+			console.log("Refresh token verification failed:", refreshError.message);
 
-	// 		const REFRESH_TOKEN = req.cookies.REFRESH_TOKEN;
-	// 		await axios.post(`http://localhost:3008/api/v1/session/delete-session`, {
-	// 			refreshToken: REFRESH_TOKEN,
-	// 		});
+			if (req.cookies.REFRESH_TOKEN) {
+				await axios.post(
+					`http://localhost:3008/api/v1/session/delete-session`,
+					{
+						refreshToken: req.cookies.REFRESH_TOKEN,
+					}
+				);
+			}
 
-	// 		res.cookie("ACCESS_TOKEN", " ", {
-	// 			httpOnly: true,
-	// 			sameSite: "None",
-	// 			secure: true,
-	// 		});
-	// 		res.cookie("REFRESH_TOKEN", " ", {
-	// 			httpOnly: true,
-	// 			sameSite: "None",
-	// 			secure: true,
-	// 		});
+			// Clear tokens
+			res.clearCookie("ACCESS_TOKEN", {
+				httpOnly: true,
+				sameSite: "None",
+				secure: true,
+			});
+			res.clearCookie("REFRESH_TOKEN", {
+				httpOnly: true,
+				sameSite: "None",
+				secure: true,
+			});
 
-	// 		return res.status(401).send("Session expired");
-	// 	}
-	// }
-	console.log("applied");
-
-	next();
+			return res.status(401).send("Session expired");
+		}
+	}
 }
 
 export { verifyJWT };
