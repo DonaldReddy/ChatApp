deaa5d0a30d2ab85a8167f74f47521b8d1aff575
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-08-10 21:37:50 +0530

    add socket sessions

--- APIGateway/services.js
+++ APIGateway/services.js
@@ -1,8 +1,15 @@
 const services = [
 	{
-		route: "/socket.io",
-		target: "http://localhost:3007/",
-		type: "ws",
+		route: "/user",
+		target: process.env.IS_DEV
+			? "http://localhost:3001/"
+			: "https://chatapp-user-service.onrender.com",
+	},
+	{
+		route: "/friend-request",
+		target: process.env.IS_DEV
+			? "http://localhost:3004/"
+			: "https://chatapp-user-service.onrender.com",
 	},
 ];
 
