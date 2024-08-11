684b379f952240f39c195ee8beb760150206a7b4
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-08-11 19:13:35 +0530

    update proxy server for websockets

--- APIGateway/services.js
+++ APIGateway/services.js
@@ -1,3 +1,29 @@
-import services from "../services.js";
+const services = {
+	user: {
+		route: "/user",
+		target: process.env.IS_DEV
+			? "http://localhost:3001"
+			: "https://chatapp-user-service.onrender.com",
+	},
+	friendRequest: {
+		route: "/friend-request",
+		target: process.env.IS_DEV
+			? "http://localhost:3004"
+			: "https://chatapp-user-service.onrender.com",
+	},
+	session: {
+		route: "/session",
+		target: process.env.IS_DEV
+			? "http://localhost:3008"
+			: "https://chatapp-user-service.onrender.com",
+	},
+	socketIO: {
+		route: "/socket.io",
+		target: process.env.IS_DEV
+			? "http://localhost:3002/socket.io"
+			: "hhttps://chatapp-websocket-service.onrender.com/socket.io",
+		type: "ws",
+	},
+};
 
 export default services;
