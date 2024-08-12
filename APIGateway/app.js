f3226219fcaf6446a3b80af4e0e6ce0d705dc9e9
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-08-12 17:15:03 +0530

    update

--- APIGateway/app.js
+++ APIGateway/app.js
@@ -25,7 +25,14 @@ proxy.on("error", (err, req, res) => {
 app.use(
 	cors({
 		credentials: true,
-		origin: /\.donaldreddy\.xyz$/,
+		origin: function (origin, callback) {
+			console.log(origin);
+			if (!origin || /\.donaldreddy\.xyz$/.test(origin)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
 	}),
 );
 
