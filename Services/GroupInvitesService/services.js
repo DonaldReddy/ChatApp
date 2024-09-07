20fac06084346487fd189966f39729a5d2935d62
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-09-07 15:11:43 +0530

    update

--- Services/GroupInvitesService/services.js
+++ Services/GroupInvitesService/services.js
@@ -1,15 +1,17 @@
 const services = {
 	chat: {
 		route: "/chat",
-		target: process.env.IS_DEV
-			? "http://localhost:3001"
-			: "https://chatapp-websocket-service.onrender.com/socket.io",
+		target:
+			process.env.IS_DEV == "true"
+				? "http://localhost:3001"
+				: "https://chat.services.hola.donaldreddy.xyz",
 	},
 	group: {
 		route: "/group",
-		target: process.env.IS_DEV
-			? "http://localhost:3004"
-			: "https://chatapp-websocket-service.onrender.com/socket.io",
+		target:
+			process.env.IS_DEV == "true"
+				? "http://localhost:3004"
+				: "https://chatapp-websocket-service.onrender.com/socket.io",
 	},
 };
 
